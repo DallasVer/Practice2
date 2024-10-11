0dac6bd07f2a4e553dfb1f1248c3b254a2fb5a9b
DallasVer <da11as.ver@mail,ru>
2024-10-11 14:29:29 +0700

    textRead_C#

---
+++ FileOperations.cs
@@ -0,0 +1,17 @@
+﻿namespace TextBox;
+
+public class FileOperations
+{
+  public string fileContent, filePath;
+
+  public string loudFile(string fileMainPath) {
+    if (File.Exists(fileMainPath)){
+      fileContent = File.ReadAllText(fileMainPath); 
+      filePath = fileMainPath;
+      return "Загрузка прошла успешно!";
+    }
+    else {
+      return "Ошибка файл не найден!";
+    }  
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TextBox;
+
+using System.Diagnostics;
+
+public class Program
+{
+  public static void Main(string[] args) {
+    Console.WriteLine("Выбирете действие \n 1. Открыть файл \n 2. Прочесть открытый файл \n 3. Заменить файл \n 4. Сохранить изменения в файл");
+    int choice = Convert.ToInt32(Console.ReadLine());
+    string filePath = " ";
+
+    switch (choice) {
+      case 1:
+        Console.Clear();
+        Console.WriteLine("Введите точный путь до файла \n(Подсказка: C:/Users/User/RiderProjects/file)");
+        filePath = Console.ReadLine();
+        
+        break;
+    }
+  }
+}
